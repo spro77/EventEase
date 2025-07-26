8acdc4b585c0cc9aba8dad386349f528055e0b5d
Sergii Prostov <spro77@gmail.com>
2025-07-25 23:40:56 -0600

    Add task management functionality with TaskService and TaskItem components

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,6 +1,9 @@
-namespace DefaultNamespace;
+namespace EventEase.Models;
 
 public class TaskItem
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EventEase;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Register your TaskService
+builder.Services.AddSingleton<TaskService>();
 
 await builder.Build().RunAsync();
